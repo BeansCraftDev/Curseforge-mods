3e2146b1b19a8b8e1321464ffc182118a974aede
PikachuGabe <84477180+PikachuGabe@users.noreply.github.com>
2025-02-05 14:44:52 -0600

    Attempt to fix some issues

--- src/main/java/com/pika/bcc/bcc.java
+++ src/main/java/com/pika/bcc/bcc.java
@@ -1,10 +1,10 @@
-package com.pika.bcm;
+package com.pika.bcc;
 
 import net.minecraftforge.fml.common.Mod;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(bcc.ID)
-public class bcm
+public class bcc
 {
     // Define mod id in a common place for everything to reference
     public static final String ID = "bcc";
